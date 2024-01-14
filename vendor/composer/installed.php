66c2ab7fd57b82d98ac550c3761786cdd675faad
Rafael de Freitas <102344043+rafarfv@users.noreply.github.com>
2024-01-14 18:32:41 -0300

    crud

---
+++ vendor/composer/installed.php
@@ -0,0 +1,23 @@
+<?php return array(
+    'root' => array(
+        'name' => 'rafarfv/rafarfv-crud-php-poo',
+        'pretty_version' => 'dev-main',
+        'version' => 'dev-main',
+        'reference' => 'b7dbcf70d5924790e3a950710eafb79c44fe24b8',
+        'type' => 'library',
+        'install_path' => __DIR__ . '/../../',
+        'aliases' => array(),
+        'dev' => true,
+    ),
+    'versions' => array(
+        'rafarfv/rafarfv-crud-php-poo' => array(
+            'pretty_version' => 'dev-main',
+            'version' => 'dev-main',
+            'reference' => 'b7dbcf70d5924790e3a950710eafb79c44fe24b8',
+            'type' => 'library',
+            'install_path' => __DIR__ . '/../../',
+            'aliases' => array(),
+            'dev_requirement' => false,
+        ),
+    ),
+);
